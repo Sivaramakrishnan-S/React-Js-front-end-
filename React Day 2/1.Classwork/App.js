106a7837761464cacc61b2262f8ab8811903f9db
Sivaramakrishnan-S <128037187+Sivaramakrishnan-S@users.noreply.github.com>
2023-04-02 17:09:13 +0530

    Create App.js

---
+++ React Day 2/1.Classwork/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import Products from './Products';
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Products/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
