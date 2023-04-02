a53b906bcf1f5781159366e6f11b1ed4f529a8ad
Sivaramakrishnan-S <128037187+Sivaramakrishnan-S@users.noreply.github.com>
2023-04-02 17:10:26 +0530

    Create Header.js

---
+++ React Day 2/1.Classwork/Header.js
@@ -0,0 +1,32 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Logo from './logo192.png'
+
+function Header(){
+    return(
+        <div>
+            <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src={Logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            Startup
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+        </div>
+    );
+}
+export default Header;
