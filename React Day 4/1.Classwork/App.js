28d2ce92863df3b6a947908abc04a85146ca26e9
Sivaramakrishnan-S <128037187+Sivaramakrishnan-S@users.noreply.github.com>
2023-04-02 17:46:04 +0530

    Create App.js

---
+++ React Day 4/1.Classwork/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Dayfour1 from './Dayfour1'
+
+import './App.css';
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <center>
+          <Dayfour1/>
+        </center>
+      </div>
+    )
+  }
+}
+export default App
